7b8969c7dffa93291a0484b0d2fda3ad7ccb6632
Nez Mustafa <nezmustafa123@gmail.com>
2022-01-08 14:07:18 +0000

    create palindrom function that takes in input value and extracts first part of the string

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector("btn");
+const result = document.querySelector(".result");
+
+btn.addEventListener("click", palindrome);
+
+//madam
+//noon
+function palindrome() {
+  const word = document.querySelector(".input-text").value;
+  let length = word.length;
+
+  let start = word.substring(0, Math.floor(len / 2));
+}
